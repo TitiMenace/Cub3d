040496e8ca85882a78e2b9e98dc0d82dd81d7eb4
greengo <sydauria@student.42.fr>
2024-02-16 14:39:54 +0100

    modified include

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -15,8 +15,8 @@
 
 # include "includes.h"
 # include "struct.h"
-# include "../lib/minilibx/mlx.h"
-# include "../lib/42_libft/libft.h"
+# include "/lib/minilibx/mlx.h"
+# include "/lib/42_libft/libft.h"
 
 /* -------------------------------------------------------------------------- */
 /*                                  PARSING                                   */
